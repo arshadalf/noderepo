bff713291fce4c97ec290c6380b0f7c4bd5ddd25
arshad <arshadalf@yahoo.com>
2021-10-15 10:35:54 +0500

    first commit

---
+++ 2-name.js
@@ -0,0 +1,4 @@
+const secret = 'SUPER SECRET'
+const john = 'John';
+const peter = 'Peter';
+module.exports = { john, peter }
---
+++ 3-module.js
@@ -0,0 +1,8 @@
+
+const names = require('./2-name');
+const sayHi = require('./3-utils');
+const data = require('./alternative-flavour');
+require('./7-mind-gernade')
+// sayHi('susan');
+// sayHi(names.john);
+// sayHi(names.peter);
